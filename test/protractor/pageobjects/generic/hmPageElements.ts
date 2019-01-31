431e6341b593724d992c0345320ecd4e9b5e5cdb
haslan <hasan.aslan@incentro.com>
2019-01-31 16:55:17 +0100

    defining elements on homepage

--- test/protractor/pageobjects/generic/hmPageElements.ts
+++ test/protractor/pageobjects/generic/hmPageElements.ts
@@ -2,5 +2,10 @@ export class HmPageElements {
 
   //INSURANCE BUTTONS HOMEPAGE
   homePageAutoverzekeringElement: string = '[href*="/autoverzekering"][class*="insurances_tabLinkAction"]';
+  homePageWoonverzekeringElement: string = '[href*="/woonverzekering"][class*="insurances_tabLinkAction"]';
+  homePageAansprakelijkheidsverzekeringElement: string = '[href*="/aansprakelijkheidsverzekering"][class*="insurances_tabLinkAction"]';
+  homePageZorgverzekeringElement: string = '[href*="/zorgverzekering"][class*="insurances_tabLinkAction"]';
+  homePageReisverzekeringElement: string = '[href*="/zorgservice/alternatievezorg"][class*="insurances_tabLinkAction"]';
+  homePageAlleverzekeringElement: string = '[href*="/verzekeringen"][class*="insurances_tabButtonAction"]';
 
 }
