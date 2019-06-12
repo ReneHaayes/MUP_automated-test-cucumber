797e9ebc20628063130e72360f5e481dbd291f79
Hasan Aslan <hasan.aslan@unive.nl>
2019-06-12 15:05:16 +0200

    WIP adding scenario for verhuizing doorgeven.

---
+++ test/protractor/pageobjects/mijnUniveParticulier/gegevens/verhuizingDoorgevenElements.ts
@@ -0,0 +1,15 @@
+export class VerhuizingDoorgevenElements {
+
+  //MIJN GEGEVENS
+  verhuizingDoorgevenClickElement: string = '[class="page-links-item__link media link"][href="/mijnunive/verhuizingdoorgeven"]';
+
+  //NIEUWE ADRESGEGEVENS
+  movingDateInputElement: string = '#date';
+  zipcodeInputElement: string = '#zipcode';
+  houseNumberInputElement: string = '#houseNumber';
+  streetInputElement: string = '#street';
+  cityInputElement: string = '#city';
+  coInsuredAppliesYesClickElement: string = '[for="coInsured__applies_0"]';
+  coInsuredAppliesNoClickElement: string = '[for="coInsured__applies_1"]';
+
+}
