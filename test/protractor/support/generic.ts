3d5563f54ef2c9dda3a6a036fc2d8b037f5ba6c9
Hasan Aslan <hasan.aslan@unive.nl>
2019-10-07 16:42:58 +0200

    [FIX] added more files to make the overview better.

---
+++ test/protractor/support/generic.ts
@@ -0,0 +1,9 @@
+import {GenericMethods} from "../pageobjects/generic/genericMethods";
+import {GenericElements} from "../pageobjects/generic/genericElements";
+import {GetUrlUnive} from "../pageobjects/generic/getUrlUnive";
+import {NawElements} from "../pageobjects/generic/nawElements";
+
+export const genericMethods: GenericMethods = new GenericMethods();
+export const genericElements: GenericElements = new GenericElements();
+export const getUrlUnive: GetUrlUnive = new GetUrlUnive();
+export const nawElements: NawElements = new NawElements();
---
+++ test/protractor/support/zakelijkElements.ts
@@ -0,0 +1,19 @@
+import {AlleVerzekeringenZakelijkElements} from "../pageobjects/zakelijkeVerzekeringen/alleVerzekeringen/alleVerzekeringenZakelijkElements";
+import {ZakelijkAansprakelijkheidsVerzekeringElements} from "../pageobjects/zakelijkeVerzekeringen/bedrijfsActiviteitenVerzekeringen/zakelijkAansprakelijkheidsVerzekeringElements";
+import {RoerendeZakenVerzekeringElements} from "../pageobjects/zakelijkeVerzekeringen/bedrijfsMiddelenVerzekeringen/roerendeZakenVerzekeringElements";
+import {EigenVervoerVerzekeringElements} from "../pageobjects/zakelijkeVerzekeringen/mobiliteitsVerzekeringen/eigenVervoerVerzekeringElements";
+import {ZakelijkTractorVerzekeringElements} from "../pageobjects/zakelijkeVerzekeringen/mobiliteitsVerzekeringen/zakelijkTractorVerzekeringElements";
+import {BedrijfsAutoVerzekeringElements} from "../pageobjects/zakelijkeVerzekeringen/mobiliteitsVerzekeringen/bedrijfsAutoVerzekeringElements";
+import {ProductPageElements} from "../pageobjects/generic/productPageElements";
+import {AovZzpElements} from "../pageobjects/zakelijkeVerzekeringen/bedrijfsActiviteitenVerzekeringen/aovZzpElements";
+import {ZakelijkRechtsBijstandVerzekeringElements} from "../pageobjects/zakelijkeVerzekeringen/bedrijfsActiviteitenVerzekeringen/zakelijkRechtsBijstandVerzekeringElements";
+
+export const alleVerzekeringenZakelijkElements: AlleVerzekeringenZakelijkElements = new AlleVerzekeringenZakelijkElements();
+export const zakelijkAansprakelijkheidsVerzekeringElements: ZakelijkAansprakelijkheidsVerzekeringElements = new ZakelijkAansprakelijkheidsVerzekeringElements();
+export const roerendeZakenVerzekeringElements: RoerendeZakenVerzekeringElements = new RoerendeZakenVerzekeringElements();
+export const eigenVervoerVerzekeringElements: EigenVervoerVerzekeringElements = new EigenVervoerVerzekeringElements();
+export const zakelijkTractorVerzekeringElements: ZakelijkTractorVerzekeringElements = new ZakelijkTractorVerzekeringElements();
+export const bedrijfsAutoVerzekeringElements: BedrijfsAutoVerzekeringElements = new BedrijfsAutoVerzekeringElements();
+export const productPageElements: ProductPageElements = new ProductPageElements();
+export const aovZzpElements: AovZzpElements = new AovZzpElements();
+export const zakelijkRechtsBijstandVerzekeringElements: ZakelijkRechtsBijstandVerzekeringElements = new ZakelijkRechtsBijstandVerzekeringElements;
