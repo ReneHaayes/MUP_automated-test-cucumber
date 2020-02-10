57456ace45f3740d6e3c1fcf4cadca56def1b4be
Hasan Aslan <hasan.aslan@unive.nl>
2020-02-10 15:12:00 +0100

    [DONE][BSP1684] Added extra scenario for checking the product pages.

--- test/protractor/features/visual/particuliereVerzekeringen/homepage.feature
+++ test/protractor/features/visual/particuliereVerzekeringen/homepage.feature
@@ -5,4 +5,4 @@ Feature: Testing of the visual appereance of the homepage
   Scenario: Compare screenshots when at the top of the homepage.
     Given I am on the Home page of the Unive website
     When The page waits for licenseplate element is visible
-    Then Verify visual elements are the same compared to the base screenshot: homepage/homePageTop
+    Then Verify visual elements are the same for home compared to the base screenshot: particuliereVerzekeringen/homepage/homePageTop
--- test/protractor/step_definitions/visual/visualTestSteps.ts
+++ test/protractor/step_definitions/visual/visualTestSteps.ts
@@ -3,10 +3,15 @@ import {genericElements, genericMethods, hmPageElements, visualMethods} from "@s
 import {browser} from "protractor";
 
 When(/^The page waits for licenseplate element is visible$/, async () => {
-  await genericMethods.waitForElementNotVisible(genericElements.cookieClickElement, browser.getPageTimeout);
   await genericMethods.waitForElementIsVisible(hmPageElements.licensePlateWidgetInputElement, browser.getPageTimeout);
 });
 
-Then(/^Verify visual elements are the same compared to the base screenshot: (.*)$/, async (baseImage: string) => {
+Then(/^Verify visual elements are the same for home compared to the base screenshot: (.*)$/, async (baseImage: string) => {
+  await genericMethods.waitForElementNotVisible(genericElements.cookieClickElement, browser.getPageTimeout);
   await visualMethods.makeScreenshotAndVerifyWithBaseline(baseImage, 'home');
+});
+
+Then(/^Verify visual elements are the same for product pages compared to the base screenshot: (.*)$/, async (baseImage: string) => {
+  await genericMethods.waitForElementNotVisible(genericElements.cookieClickElement, browser.getPageTimeout);
+  await visualMethods.makeScreenshotAndVerifyWithBaselineSetSize(baseImage, 1920, 12500);
 });
