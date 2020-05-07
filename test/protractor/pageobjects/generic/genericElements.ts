3c57d39e2a668c6620be7e7cc783726cf9c9579d
jjvanderkam <bLM24Q3c4LfPWags>
2020-05-07 11:43:58 +0200

    Update productPageZakelijkVerzekering.feature - Extra kolom toegevoegd Feature file. - Extra variabel toegevoegd.

--- test/protractor/pageobjects/generic/genericElements.ts
+++ test/protractor/pageobjects/generic/genericElements.ts
@@ -2,7 +2,7 @@ export class GenericElements {
   alleVerzekeringen: string = 'verzekeringen';
   alleVerzekeringenZakelijk: string = 'zakelijk/verzekeringen';
   autoVerzUrlPat: string = 'autoverzekering/premieberekenen-en-afsluiten';
-  aovZzpPat: string = 'zakelijk/arbeidsongeschiktheidsverzekering-zzp/premieberekenen-en-afsluiten/uw-aov';
+  aovZzpPat: string = 'zakelijk/arbeidsongeschiktheidsverzekering/aov-zzp/premieberekenen-en-afsluiten/uw-aov';
   aansprakelijkheidsVerzekeringPat: string = 'aansprakelijkheidsverzekering/premieberekenen-en-afsluiten';
   autoVerzProductPagePat: string = 'autoverzekering';
   woonVerzPat: string = 'woonverzekering/premieberekenen-en-afsluiten';
