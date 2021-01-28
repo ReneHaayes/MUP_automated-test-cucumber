072c9536f51d7becacf9a8d3231a12028fe81453
jjvanderkam <bLM24Q3c4LfPWags>
2021-01-28 16:24:11 +0100

    Toevoegen closevirtualtour Gebruiker illing aangepast voor claimdamage Nieuwe schadenummers toegevoegd.

--- test/protractor/pageobjects/generic/genericElements.ts
+++ test/protractor/pageobjects/generic/genericElements.ts
@@ -10,7 +10,7 @@ export class GenericElements {
   bromfietsVerzekeringPat = 'bromfietsverzekering/premieberekenen-en-afsluiten';
   scooterVerzekeringPat = 'scooterverzekering/premieberekenen-en-afsluiten';
   motorVerzekeringPat = 'motorverzekering/premieberekenen-en-afsluiten';
-  quadVerzekeringPat = 'motorverzekering/premieberekenen-en-afsluiten-quad';
+  quadVerzekeringPat = 'motorverzeFkering/premieberekenen-en-afsluiten-quad';
   trikeVerzekeringPat = 'motorverzekering/premieberekenen-en-afsluiten-trike';
   zakelijkMotorfietsVerzekering = 'zakelijk/motorverzekering';
   zorgeloosOnlineVerzekeringPat = 'zorgeloosonline/premieberekenen-en-afsluiten';
