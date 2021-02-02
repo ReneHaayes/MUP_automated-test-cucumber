601eb14169001b60217938c2d0590a59d9b75ab6
Peter van der Woude <peter.vanderwoude@polteq.com>
2021-02-02 10:50:33 +0100

    Aanvullen develop: Widgets implementeren

--- test/protractor/pageobjects/generic/genericElements.ts
+++ test/protractor/pageobjects/generic/genericElements.ts
@@ -96,6 +96,8 @@ export class GenericElements {
   prefillOmringPashoudersUrl = 'autoverzekering/premieberekenen-en-afsluiten?internetcode=vG71jD90&collectiefSoort=internetCode';
   salesWidgets = 'ppwidgets';
   zzpPakket = 'zakelijk/zzp/pakket-bouw/premieberekenen-en-afsluiten';
+  salesWidgets = 'ppwidgets';
+
   faqPageWatIsEenWaAutoVerzekering = '/autoverzekering/wa';
 
   zorgchecktool = 'zorgverzekering/zorgcheck';
