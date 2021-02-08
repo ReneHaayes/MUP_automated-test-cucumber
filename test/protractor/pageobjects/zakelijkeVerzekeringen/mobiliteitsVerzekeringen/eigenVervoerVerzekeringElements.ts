1a3772542d87629cf2e9b20f1aac29f62673df5b
Peter van der Woude <peter.vanderwoude@polteq.com>
2021-02-08 12:20:33 +0100

    Nieuwe workflow voor Eigen vervoersverzekering. Op Pagina 1 word nu gevraagd om een KVK nummer

--- test/protractor/pageobjects/zakelijkeVerzekeringen/mobiliteitsVerzekeringen/eigenVervoerVerzekeringElements.ts
+++ test/protractor/pageobjects/zakelijkeVerzekeringen/mobiliteitsVerzekeringen/eigenVervoerVerzekeringElements.ts
@@ -38,11 +38,10 @@ export class EigenVervoerVerzekeringElements {
   activitySelectWebdesignElement = 'string:webdesign';
   activitySelectOverigElement = 'string:overig';
 
-  correctActivityYesElement = '[data-label-id="LA_IF31406_39608"] > div > label:nth-child(1)' ;     // '#_Form_IF31406_39608_true"';
-  correctActivityNoElement = '[data-label-id="LA_IF31406_39608"] > div > label:nth-child(2)' ;      // '#_Form_IF31406_39608_false';
-  hasEmployeeYesElement = '[data-label-id="LA_IF31406_37329"] > div > label:nth-child(1)';          // '#_Form_IF31406_37329_true';
-  hasEmployeeNoElement =  '[data-label-id="LA_IF31406_37329"] > div > label:nth-child(2)' ;         // '#_Form_IF31406_37329_false';
-
+  correctActivityYesElement = '[data-label-id="LA_IF31406_39608"] div label:nth-child(1)';
+  correctActivityNoElement = '[data-label-id="LA_IF31406_39608"] div label:nth-child(2)';
+  hasEmployeeYesElement = 'div[data-label-id="LA_IF31406_37329"] > div > label:nth-child(1)';
+  hasEmployeeNoElement = 'div[data-label-id="LA_IF31406_37329"] > div > label:nth-child(2)';
 
   // VEHICLE INFO
   vehicleInfoPassengerCarClickElement = '[data-label-id*="LA_IF21624_28332"] label';
