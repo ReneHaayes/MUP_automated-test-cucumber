0fa46f0da2ffebb01dd69c884a45c964a1f4f503
Peter van der Woude <peter.vanderwoude@polteq.com>
2021-02-08 12:20:33 +0100

    Nieuwe workflow voor Eigen vervoersverzekering. Op Pagina 1 word nu gevraagd om een KVK nummer

--- test/protractor/step_definitions/zakelijkeVerzekeringen/mobiliteitsVerzekeringen/eigenVervoerVerzekeringSteps.ts
+++ test/protractor/step_definitions/zakelijkeVerzekeringen/mobiliteitsVerzekeringen/eigenVervoerVerzekeringSteps.ts
@@ -56,12 +56,33 @@ When(/^I enter the first step of Eigen vervoersverzekering with (.*) and click n
 
   When(/^I answer the questions on step one with:$/, async function (data) {
     const dataTable = data.rowsHash();
-   //  await eigenVervoerVerzekeringMethods.selectCorrectCompanyActivity(dataTable.correctCompanyActivity);
-   // await eigenVervoerVerzekeringMethods.selectHasEmployees(dataTable.hasEmployee);
+     // await eigenVervoerVerzekeringMethods.selectCorrectCompanyActivity(dataTable.correctCompanyActivity);
+     // await eigenVervoerVerzekeringMethods.selectHasEmployees(dataTable.hasEmployee);
+
     await genericMethods.clickOnElement(eigenVervoerVerzekeringElements.correctActivityYesElement);
-    await genericMethods.clickOnElement(eigenVervoerVerzekeringElements.hasEmployeeNoElement);
+    await genericMethods.clickOnElement(eigenVervoerVerzekeringElements.hasEmployeeYesElement);
+
     await eigenVervoerVerzekeringMethods.selectVehicleInfo(dataTable.typeCar, dataTable.howMany);
+    await genericMethods.waitForElementIsVisible(eigenVervoerVerzekeringElements.openTrailerNoClickElement, browser.getPageTimeout);
     await genericMethods.clickOnElement(eigenVervoerVerzekeringElements.openTrailerNoClickElement);
     await genericMethods.clickOnNextButton();
     await genericMethods.clickOnNextButton();
   });
+
+When(/^I enter the third step of eigen vervoer verzekering for (.*) with (.*)$/, async (company: string, persona: string) => {
+  await genericMethods.verifyTextInElement(eigenVervoerVerzekeringElements.kvkNumberPrefilledElement, companyData.getCompanyKvkNumber(company));
+  await genericMethods.verifyTextInElement(eigenVervoerVerzekeringElements.companyNamePrefilledElement, companyData.getCompanyName(company));
+  await genericMethods.typeText(nawElements.companyDataPhoneNumberInputElement, companyData.getCompanyPhoneNumber(company));
+  await genericMethods.typeText(nawElements.companyDataEmailAddressInputElement, companyData.getCompanyEmailAddress(company));
+  await genericMethods.clickOnTAB(nawElements.companyDataEmailAddressInputElement);
+  await genericMethods.typeText(nawElements.companyDataEmailAddressConfirmInputElement, companyData.getCompanyEmailAddress(company));
+  await genericMethods.clickOnTAB(nawElements.companyDataEmailAddressConfirmInputElement);
+  await genericMethods.typeText(nawElements.contactDataInitialsInputElement, personaData.getPersonaInitials(persona));
+  await genericMethods.typeText(nawElements.contactDataPrefixInputElement, personaData.getPersonaPrefix(persona));
+  await genericMethods.typeText(nawElements.contactDataLastNameInputElement, personaData.getPersonaLastName(persona));
+  await genericMethods.clickContactDataGender(personaData.getPersonaGender(persona));
+  await genericMethods.typeText(genericElements.accountNumberElement, personaData.getPersonaAccountNumber(persona));
+  await genericMethods.clickOnTAB(genericElements.accountNumberElement);
+  await genericMethods.clickOnElement(genericElements.authorizationUniveElement);
+  await genericMethods.clickOnNextButton();
+});
