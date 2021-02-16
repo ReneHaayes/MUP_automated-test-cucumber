3c6400ceb766fdcf6c9597357a5e93df3261e2fb
Peter van der Woude <peter.vanderwoude@polteq.com>
2021-02-16 15:12:32 +0100

    Commit voor ZZP pakket

--- test/protractor/pageobjects/generic/genericElements.ts
+++ test/protractor/pageobjects/generic/genericElements.ts
@@ -87,7 +87,6 @@ export class GenericElements {
   salesWidgets = 'ppwidgets';
   zzpPakket = 'zakelijk/zzp/pakket-bouw/premieberekenen-en-afsluiten';
 
-  zzpPakket = 'zakelijk/zzp/pakket-bouw/premieberekenen-en-afsluiten';
   faqPageWatIsEenWaAutoVerzekering = '/autoverzekering/wa';
 
   zorgchecktool = 'zorgverzekering/zorgcheck';
