87cdddafc714d9a1c331cdbbdd2d4a5568e553d8
PQTE179\Gebruiker <rene.haayes@polteq.com>
2021-07-08 13:38:32 +0200

    klantenserviceDirectOnlineRegelenElements.ts fixed whitespace

--- test/protractor/pageobjects/Klantenservice/klantenserviceDirectOnlineRegelenElements.ts
+++ test/protractor/pageobjects/Klantenservice/klantenserviceDirectOnlineRegelenElements.ts
@@ -1,7 +1,7 @@
 export class KlantenserviceDirectOnlineRegelenElements {
 
   directOnlineRegelenTileClickElement(input: string): string {
-    return 'main-container[backgroundimage="green-quarter-unibol"] [text="'+input+'"]';
+    return 'main-container[backgroundimage="green-quarter-unibol"] [text="' + input + '"]';
   }
 
 }
